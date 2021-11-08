c6325cbd7cada8f85d5e5083e2ee3c13d02cde1e
Seongjae <robbyl95@hanmail.net>
2021-11-08 09:23:14 +0900

    seongjae_start

---
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -0,0 +1,264 @@
+import React, { useState, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { registerUser } from "../../../actions/user_actions";
+import { Controller, useForm } from "react-hook-form";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+function Copyright(props) {
+    return (
+        <Typography
+            variant="body2"
+            color="text.secondary"
+            align="center"
+            {...props}
+        >
+            {"Copyright © "}
+            <Link color="inherit" href="https://mui.com/">
+                UsEarth
+            </Link>{" "}
+            {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    );
+}
+
+const theme = createTheme();
+
+const RegisterPage = (props) => {
+    const dispatch = useDispatch();
+
+    // const { handleSubmit, watch, reset, control } = useForm();
+    // console.log(watch("email"));
+
+    const [Email, setEmail] = useState("");
+    const [Password, setPassword] = useState("");
+    const [ConfirmPassword, setConfirmPassword] = useState("");
+    const [Name, setName] = useState("");
+    const [Nickname, setNickname] = useState("");
+    const [Address, setAddress] = useState("");
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value);
+    };
+    const onNameHandler = (event) => {
+        setName(event.currentTarget.value);
+    };
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    };
+    const onConfirmPasswordHandler = (event) => {
+        setConfirmPassword(event.currentTarget.value);
+    };
+    const onNicknameHandler = (event) => {
+        setNickname(event.currentTarget.value);
+    };
+    const onAddressHandler = (event) => {
+        setAddress(event.currentTarget.value);
+    };
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        if (Password !== ConfirmPassword) {
+            return alert("비밀번호와 비밀번호 확인은 같아야 합니다.");
+        }
+
+        let body = {
+            email: Email,
+            password: Password,
+            name: Name,
+            nickname: Nickname,
+            address: Address,
+        };
+
+        dispatch(registerUser(body)).then((response) => {
+            if (response.payload.success) {
+                props.history.push("/login");
+            } else {
+                alert("Failed to sign up");
+            }
+        });
+    };
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                height: "100vh",
+            }}
+        >
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Typography component="h1" variant="h5">
+                            회원가입
+                        </Typography>
+                        <Box
+                            component="form"
+                            noValidate
+                            onSubmit={onSubmitHandler}
+                            sx={{ mt: 3 }}
+                        >
+                            <Grid container spacing={1}>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        value={Email}
+                                        onChange={onEmailHandler}
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                        autoFocus
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        value={Password}
+                                        onChange={onPasswordHandler}
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="new-password"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        value={ConfirmPassword}
+                                        onChange={onConfirmPasswordHandler}
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Confirm Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="new-password"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        value={Name}
+                                        onChange={onNameHandler}
+                                        required
+                                        fullWidth
+                                        id="Name"
+                                        label="Name"
+                                        name="Name"
+                                        autoComplete="name"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        value={Nickname}
+                                        onChange={onNicknameHandler}
+                                        required
+                                        fullWidth
+                                        id="nickname"
+                                        label="Nickname"
+                                        name="nickname"
+                                        autoComplete="nickname"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        value={Address}
+                                        onChange={onAddressHandler}
+                                        required
+                                        fullWidth
+                                        id="address"
+                                        label="Address"
+                                        name="address"
+                                        autoComplete="address"
+                                    />
+                                </Grid>
+
+                                <Grid item xs={12}>
+                                    <FormControlLabel
+                                        control={
+                                            <Checkbox
+                                                value="allowExtraEmails"
+                                                color="primary"
+                                            />
+                                        }
+                                        label="I want to receive inspiration, marketing promotions and updates via email."
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign Up
+                            </Button>
+                            <Grid container justifyContent="flex-end">
+                                <Grid item>
+                                    <Link href="/login" variant="body2">
+                                        Already have an account? Sign in
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                    <Copyright sx={{ mt: 5 }} />
+                </Container>
+            </ThemeProvider>
+            {/* <form
+                style={{ display: "flex", flexDirection: "column" }}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+
+                <label>Name</label>
+                <input type="text" value={Name} onChange={onNameHandler} />
+
+                <label>Password</label>
+                <input
+                    type="password"
+                    value={Password}
+                    onChange={onPasswordHandler}
+                />
+
+                <label>Confirm Password</label>
+                <input
+                    type="password"
+                    value={ConfirmPassword}
+                    onChange={onConfirmPasswordHandler}
+                />
+                <br />
+
+                <button type="submit">회원가입</button>
+            </form> */}
+        </div>
+    );
+};
+
+export default withRouter(RegisterPage);
---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://seongjae:1234@sjdb.eipk6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+};
