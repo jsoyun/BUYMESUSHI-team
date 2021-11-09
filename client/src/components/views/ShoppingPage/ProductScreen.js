1331c756d10b47b4c3a63fdb9a23690e1f2337c7
jsoyun <syusmm233@gmail.com>
2021-11-09 17:14:45 +0900

    ProductScreen

--- client/src/components/views/ShoppingPage/ProductScreen.js
+++ client/src/components/views/ShoppingPage/ProductScreen.js
@@ -1,7 +1,27 @@
 import React from "react";
+import "./ProductScreen.css";
 
 const ProductScreen = () => {
-  return <div></div>;
+  return (
+    <div className="productscreen">
+      <div className="productscreen_left">
+        <div className="left_image">
+          <img
+            src="https://cdn.pixabay.com/photo/2013/10/03/23/19/bike-190483__340.jpg"
+            alt="product name"
+          ></img>
+        </div>
+      </div>
+      <div className="left_info">
+        <p className="left_name">product1</p>
+        <p>price:$499</p>
+        <p>description: 블라블라블라</p>
+      </div>
+      <div className="productscreen_right">
+        <div className></div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductScreen;
