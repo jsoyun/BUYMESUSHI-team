57464be18c5837a4934d4713ebef8706000fba89
jsoyun <syusmm233@gmail.com>
2021-11-09 15:40:02 +0900

    delete

--- client/src/components/views/AboutPage/About.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const About = () => {
-    return <div>About페이지</div>;
-};
-
-export default About;
--- client/src/components/views/UsEarthPage/UsEarth.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UsEarth = () => {
-    return <div>지키미인증 페이지 </div>;
-};
-
-export default UsEarth;
