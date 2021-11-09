bf2e401317423b0fce95a79224ba9b4772f490f6
Rina <fghjiff8@gmail.com>
2021-11-09 15:22:28 +0900

    landing시작페이지 주석처리OK

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,34 +1,31 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import { withRouter } from 'react-router-dom';
-import SliderPage from '../SliderPage/SliderPage';
-import ApiPage from '../ApiPage/ApiPage';
+import React, { useEffect } from "react";
+import axios from "axios";
+import { withRouter } from "react-router-dom";
+import SliderPage from "../SliderPage/SliderPage";
+import ApiPage from "../ApiPage/ApiPage";
 
 const LandingPage = (props) => {
-    useEffect(() => {
-        axios.get('/api/hello').then((response) => console.log(response.data));
-    }, []);
-    // paging은 App.js에서 라우팅하는 방식으로 해결.
-    // main 첫페이지는 헤더와 두개의 컴포넌트로 구성(SliderPage(사진 뜨는), ApiPage(아래 네브바처럼))
-    // main 두번째 페이지도 헤더와 두개의 컴포넌트로 구성(추후 회의)
-    // axios로 api 불러와놓고
+  useEffect(() => {
+    axios.get("/api/hello").then((response) => console.log(response.data));
+  }, []);
+  // paging은 App.js에서 라우팅하는 방식으로 해결.
+  // main 첫페이지는 헤더와 두개의 컴포넌트로 구성(SliderPage(사진 뜨는), ApiPage(아래 네브바처럼))
+  // main 두번째 페이지도 헤더와 두개의 컴포넌트로 구성(추후 회의)
+  // axios로 api 불러와놓고
 
+  // const onClickHandler = () => {
+  //     axios.get('/api/users/logout').then((response) => {
+  //         if (response.data.success) {
+  //             props.history.push('/login');
+  //         } else {
+  //             alert('로그아웃 하는데 실패 했습니다.');
+  //         }
+  //     });
+  // };
 
-    // const onClickHandler = () => {
-    //     axios.get('/api/users/logout').then((response) => {
-    //         if (response.data.success) {
-    //             props.history.push('/login');
-    //         } else {
-    //             alert('로그아웃 하는데 실패 했습니다.');
-    //         }
-    //     });
-    // };
-
-
-
-    return (
-        <>
-            <div
+  return (
+    <>
+      {/* <div
                 style={{
                     display: 'flex',
                     justifyContent: 'center',
@@ -39,21 +36,22 @@ const LandingPage = (props) => {
             >
                 <h1>시작 페이지</h1>
             </div>
-            <br />
-            <SliderPage />
-            <br />
-            <br />
-            <br />
-            <ApiPage
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: '100%',
-                    height: '20vh',
-                }} />
-        </>
-    );
+            <br /> */}
+      <SliderPage />
+      <br />
+      <br />
+      <br />
+      <ApiPage
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "20vh",
+        }}
+      />
+    </>
+  );
 };
 
 export default withRouter(LandingPage);
