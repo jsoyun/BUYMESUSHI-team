644f5b767d80b23cd3ac62d55113b3667216e4d2
jsoyun <syusmm233@gmail.com>
2021-11-09 18:59:16 +0900

    productScreen css

--- client/src/components/views/ShoppingPage/ProductScreen.js
+++ client/src/components/views/ShoppingPage/ProductScreen.js
@@ -18,7 +18,26 @@ const ProductScreen = () => {
         <p>description: 블라블라블라</p>
       </div>
       <div className="productscreen_right">
-        <div className></div>
+        <div className="right_info">
+          <p>
+            price:<span>$499.99</span>
+          </p>
+          <p>
+            Status:<span>In stock</span>
+          </p>
+          <p>
+            Qty
+            <select>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+          </p>
+          <p>
+            <button type="button">Add to cart</button>
+          </p>
+        </div>
       </div>
     </div>
   );
