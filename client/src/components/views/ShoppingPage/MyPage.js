0167e0335d56bcdd4b08b0f244a6a8dfb6d3799c
jsoyun <syusmm233@gmail.com>
2021-11-10 11:39:53 +0900

    mypage css

--- client/src/components/views/ShoppingPage/MyPage.js
+++ client/src/components/views/ShoppingPage/MyPage.js
@@ -1,4 +1,5 @@
 //cart
+import "./MyPage.css";
 import React from "react";
 import CartItem from "./ShopComponents/CartItem";
 
@@ -8,15 +9,18 @@ const MyPage = () => {
       <div className="cartscreen_left">
         <h2>Shopping Cart</h2>
         <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
       </div>
       <div className="cartscreen_right">
         <div className="cartscreen_info">
           <p>subtotal (0) items</p>
           <p>$499.99</p>
         </div>
-      </div>
-      <div>
-        <button>Proceed To Checkout</button>
+        <div>
+          <button>Proceed To Checkout</button>
+        </div>
       </div>
     </div>
   );
