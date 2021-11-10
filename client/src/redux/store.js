a6e93ce1272ddf9999a91537e83b25e71979cd54
jsoyun <syusmm233@gmail.com>
2021-11-10 13:51:39 +0900

    store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,3 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { composewit } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers({});
+
+const middleware = [thunk];
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
