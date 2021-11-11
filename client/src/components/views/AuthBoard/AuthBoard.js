0a99e761587aa95347ef7d6501c385b1dfae66bd
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2021-11-11 16:29:14 +0900

    delete comments

--- client/src/components/views/AuthBoard/AuthBoard.js
+++ client/src/components/views/AuthBoard/AuthBoard.js
@@ -2,10 +2,23 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { postAuthBoard } from "../../../actions/authBoard_actions";
 import Post from "./Sections/Post";
 
 const AuthBoard = (props) => {
     const dispatch = useDispatch();
+    // dispatch(postAuthBoard()).then((response) => {
+    //     if (response.payload.postAuthBoard) {
+    //         if (response.payload.postAuthBoard) {
+    //             props.history.push("/authboard");
+    //         } else {
+    //             console.log("post fail!!!");
+    //             alert("post fail!!! check description and picture!");
+    //         }
+    //     } else {
+    //         console.error("error");
+    //     }
+    // });
 
     useEffect(() => {
         axios
--- server/routes/authBoard.js
+++ server/routes/authBoard.js
@@ -8,58 +8,6 @@ const path = require("path");
 const { auth } = require("../middleware/auth");
 const router = express.Router();
 
-// try {
-//     fs.readdirSync('client/public/img/uploads');
-// } catch (error) {
-//     console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-//     fs.mkdirSync('client/public/img/uploads');
-// }
-
-//////////////////////
-
-// const upload = multer({
-//     storage: multer.diskStorage({
-//         destination(req, file, cb) {
-//             cb(null, "uploads/");
-//         },
-//         filename(req, file, cb) {
-//             const ext = path.extname(file.originalname);
-//             cb(
-//                 null,
-//                 Date.now() +
-//                     path.basename(file.originalname, ext) +
-//                     "_" +
-//                     writer +
-//                     ext
-//             );
-//         },
-//     }),
-//     limits: { fileSize: 5 * 1024 * 1024 },
-// });
-
-// const storageEngine = multer.diskStorage({
-//     destination: '/client/public/img/uploads',
-//     filename: function (req, file, callback) {
-//         callback(
-//             null,
-//             file.fieldname + '-' + Date.now() + path.extname(file.originalname)
-//         );
-//     },
-// });
-// const fileFilter = (req, file, callback) => {
-//     let pattern = /jpg|png|svg/; // reqex
-
-//     if (pattern.test(path.extname(file.originalname))) {
-//         callback(null, true);
-//     } else {
-//         callback('Error: not a valid file');
-//     }
-// };
-
-// const upload = multer({
-//     storage: storageEngine,
-//     fileFilter,
-// });
 const storageEngine = multer.diskStorage({
     destination: "client/public/img/authBoard",
     filename: function (req, file, callback) {
@@ -93,10 +41,6 @@ router.get("/", async (req, res) => {
     }
 });
 
-// app.post("/upload", upload.single("uploadedFile"), (req, res) => {
-//     res.json(req.file).status(200);
-// });
-
 router.post("/post", upload.single("authBoardPhoto"), async (req, res) => {
     try {
         // 아래 : Object: null prototype 삭제
@@ -111,12 +55,6 @@ router.post("/post", upload.single("authBoardPhoto"), async (req, res) => {
             photo: `${req.file.destination}/${req.file.filename}`,
         };
 
-        // const authBoard = new AuthBoard(req.body);
-        // authBoard.save(async (err, userInfo) => {
-        //     if (err) return res.json({ err });
-        //     return res.status(200).json({ postAuthBoard: true });
-        // });
-        // console.log(authBoard);
         AuthBoard.insertMany(insertMongo)
             .then(() => {
                 return res.status(200).json({ postAuthBoard: true });
